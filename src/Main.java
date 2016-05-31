6ecbd462cf223390da64f24e6998910c173d3631
alfa04 <joao.carlos.costa@fe.up.pt>
2016-05-31 23:47:09 +0100

    Add charts

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import org.jfree.ui.RefineryUtilities;
+
+import charts.BarChart;
+import charts.PieChart;
 import genetic.DecisionTree;
 
 public class Main {
 
 	public static void main(String[] args) {
-		DecisionTree tree = new DecisionTree();
-		System.out.println(tree);
+		/*DecisionTree tree = new DecisionTree();
+		System.out.println(tree);*/
+        PieChart pie = new PieChart("Comparison", "Which operating system are you using?");
+        pie.pack();
+        pie.setVisible(true);
+        final BarChart bar = new BarChart("Bar Chart Demo");
+        bar.pack();
+        RefineryUtilities.centerFrameOnScreen(bar);
+        bar.setVisible(true);
 	}
 }
